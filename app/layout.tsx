08e68636043454915ba7d4019a57bf489bb46d56
jovem-programador <andersonmarley011@gmail.com>
2025-07-24 14:09:31 -0300

    Update layout.tsx
    Alterando configurações

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import ClientLayout from '@/components/ClientLayout'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
