16dce9597d60ecd2105a1674efea1a9fb01cd7db
Anderson Marley <andersonmarley011@gmail.com>
2025-11-19 09:56:14 -0300

    feat: implementação do chatwoot

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppWrapper from "@/components/appWrapper";
 import AnalyticsListener from "./analytics-listener";
+import Script from 'next/script';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,12 +25,38 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const CHATWOOT_BASE_URL = "http://192.168.15.101:3001";
+  const CHATWOOT_TOKEN = 'QZE8T7cqYSoWbyG19CpDVApz';
+
   return (
     <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AnalyticsListener/>
         <AppWrapper>{children}</AppWrapper>
       </body>
+      <Script
+        id="chatwoot-script"
+        // 'lazyOnload' é ideal para widgets de chat
+        strategy="lazyOnload" 
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d,t) {
+              var BASE_URL="${CHATWOOT_BASE_URL}";
+              var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+              g.src=BASE_URL+"/packs/js/sdk.js";
+              g.async = true;
+              s.parentNode.insertBefore(g,s);
+              g.onload=function(){
+                window.chatwootSDK.run({
+                  websiteToken: '${CHATWOOT_TOKEN}',
+                  baseUrl: BASE_URL
+                })
+              }
+            })(document,"script");
+          `,
+        }}
+      />
     </html>
   );
 }
