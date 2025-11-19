640e1cce79ae48707519a693cb7a2e9ec4a701c2
Hugoritimo <hugoritimo@gmail.com>
2025-11-19 15:54:17 -0300

    Mudanças

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import AppWrapper from "@/components/appWrapper";
 import AnalyticsListener from "./analytics-listener";
 import "./globals.css";
+// 👇 ADICIONADO: Import do componente
+import ChatwootWidget from "@/components/ChatwootWidget"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AnalyticsListener/>
         <AppWrapper>{children}</AppWrapper>
+        {/* 👇 ADICIONADO: O widget entra aqui no final */}
+        <ChatwootWidget /> 
       </body>
     </html>
   );
---
+++ frontend/components/ChatwootWidget.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const ChatwootWidget = () => {
+  useEffect(() => {
+    // Configurações extraídas da sua imagem
+    const WEBSITE_TOKEN = 'QZE8T7cqYSoWbyG19CpDVApz';
+    const BASE_URL = 'http://192.168.15.101:3001';
+
+    // Função para carregar o script do Chatwoot
+    (function(d, t) {
+      var g = d.createElement(t) as HTMLScriptElement;
+      var s = d.getElementsByTagName(t)[0];
+      g.src = BASE_URL + "/packs/js/sdk.js";
+      g.defer = true;
+      g.async = true;
+      s.parentNode?.insertBefore(g, s);
+
+      g.onload = function() {
+        // @ts-ignore
+        window.chatwootSDK.run({
+          websiteToken: WEBSITE_TOKEN,
+          baseUrl: BASE_URL
+        });
+      };
+    })(document, "script");
+  }, []);
+
+  return null;
+};
+
+export default ChatwootWidget;
